a107b7a00c757d6dc89ea2658415cbfd0d0607e6
Hans Martin <hansmrtn@gmail.com>
2019-11-01 11:38:08 -0700

    added adjustRange function

--- R/utils-grid.R
+++ R/utils-grid.R
@@ -1,12 +1,12 @@
 #' @export
-#' 
+#'
 #' @title Test for correct structure in a \emph{bs_grid} object
-#' 
+#'
 #' @param bs_grid \emph{bs_grid} object
-#' 
-#' @return \code{TRUE} if \code{bs_grid} has the correct structure, 
+#'
+#' @return \code{TRUE} if \code{bs_grid} has the correct structure,
 #' \code{FALSE} otherwise.
-#' 
+#'
 #' @description The \code{bs_grid} is checked for the 'bs_grid' class name
 #' and presence of the following elements:
 #' \itemize{
@@ -24,20 +24,51 @@
 grid_isGrid <- function(
   bs_grid = NULL
 ) {
-  
+
   # ----- Validate parameters --------------------------------------------------
-  
+
   if ( is.null(bs_grid) ) return(FALSE)
   if ( !"bs_grid" %in% class(bs_grid) ) return(FALSE)
-  
+
   elements <- c(
     "longitude", "latitude", "elevation", "time", "data",
     "model", "modelRun", "deltaLon", "deltaLat"
   )
-  
+
   if ( !all(elements %in% names(bs_grid)) ) return(FALSE)
-  
+
   # Nothing failed so return TRUE
   return(TRUE)
-  
+
+}
+
+#' @keywords internal
+#' @export
+#' @title Create a Scaled Range
+#' @param x vector of numeric values
+#' @param factor scaling factor to be applied
+#' @param minRange minimum range to be returned
+#' @description Scale the range of values by a given factor. The min and max values of the
+#' original range of valus are adjusted so that the new range is larger or smaller by \code{factor}.
+#' The returned range will always be equal to or greater than \code{minRange}.
+#' @return low and high values of the adjusted range
+#' @examples
+#' \dontrun{
+#' x <- (1:10)
+#' range(x)
+#' [1] 1 10
+#' x_new <- adjustRange(x, factor = 2)
+#' range(x_new)
+#' [1] -3.5 14.5
+#' }
+adjustRange <- function(x, factor = 1.1, minRange = 0.1) {
+  scaledWidth <- diff(range(x)) * (factor - 1)
+  low <- range(x)[1] - scaledWidth / 2
+  high <- range(x)[2] + scaledWidth / 2
+  if ( (high - low) < minRange ) {
+    mid <- (max(x)-min(x))/2 + min(x)
+    low <- mid - minRange / 2
+    high <- mid + minRange / 2
+  }
+  return(c(low, high))
 }
