fb098f5682238846599fa446a16f7852ad86d860
Hans Martin <hansmrtn@gmail.com>
2019-11-21 15:44:13 -0800

    init commit

--- R/monitor_toRaster.R
+++ R/monitor_toRaster.R
@@ -1,24 +1,51 @@
+#' @export
+#' @title Create a Raster from monitor object
+#'
+#' @param ws_monitor A ws_monitor object.
+#' @param res Set the resolution of a Raster* object
+#' @param crs Set the coordinate reference system (CRS) of a Raster* object.
+#' @param extent Set or inherit the extent of the Raster* object.
+#' @param projectTo
+#' @param na.action
+#'
+#' @return
 monitor_toRaster <- function(
   ws_monitor,
   res,
   crs = '+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0',
   extent = NULL,
-  from = NULL
+  projectTo = NULL
 ) {
 
-  roundCell <- function(x, r) ceiling(x*(1/r))/(1/r)
-  M <- data.frame( x = roundCell(ws_monitor$meta$longitude, res),
-                   y = roundCell(ws_monitor$meta$latitude, res),
-                   z = t(ws_monitor$data)[-1,] )
-  ras <- raster::rasterFromXYZ(M)
+  # Checks
+  if ( ncol(ws_monitor$data) == 2 ) {
+    stop('Needs more than a single ws_monitor object.')
+  }
+  if ( PWFSLSmoke::monitor_isEmpty(ws_monitor) ) {
+    stop('Cannot be an empty ws_monitor object.')
+  }
 
-  #tm <- strftime(ws_monitor$data$datetime, '%Y-%m-%d %H:%M', tz = 'UTC')
-  #names(ras) <- tm
+  # Round the cells
+  roundCell <- function(x, r) ceiling(x*(1/r))/(1/r)
+
+  M <- data.frame(x = roundCell(ws_monitor$meta$longitude, res),
+                  y = roundCell(ws_monitor$meta$latitude, res),
+                  z = t(ws_monitor$data[,-1]) )
+  ras <- raster::rasterFromXYZ(xyz = M)
+
+  names(ras) <- as.numeric(ws_monitor$data$datetime)
+
   if ( !is.null(crs) ) raster::crs(ras) <- crs
   if ( !is.null(extent) ) raster::extent(ras) <- extent
 
-  if ( !is.null(from) ) {
-    ras <- raster::projectRaster(from = ras, to = from)
+  if ( !is.null(projectTo) ) {
+    ras <- raster::projectRaster(from = ras, to = projectTo)
   }
   return(ras)
+  if (FALSE) {
+    ws_monitor <- yuba
+    res <- 0.5
+    crs <- '+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0'
+    projectTo <- bs_raster
+  }
 }
