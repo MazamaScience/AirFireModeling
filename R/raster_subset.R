d2647f7220b7882709f2f439e7ec59ce237f42ea
Hans Martin <hansmrtn@gmail.com>
2019-12-06 15:10:52 -0800

    added polygon subsetting

--- R/raster_subset.R
+++ R/raster_subset.R
@@ -2,40 +2,77 @@
 #' @title Subset a Raster* by distance from a location
 #'
 #' @param raster A Raster* object.
-#' @param longitude A target longitude.
-#' @param latitude A target latitude.
-#' @param radius A radius to subset by (in meters)
+#' @param ... Subsetting arguments
 #'
 #' @return a Raster* object
+#'
+#' @examples
+#' \dontrun{
+#' # Load Bluesky
+#' bs <- bluesky_load()
+#'
+#' # Subset by radius
+#' raster_subset(bs, radius = 20000, latitude = 45.5, longitude = -123.32)
+#'
+#' # Subset with polygon
+#' # Create CA polygon
+#' ca <- maps::map('state', 'CA', fill = TRUE)
+#' ca_polygon <- maptools::map2SpatialPolygons(ca, IDs = ca$names, proj4string = raster::crs(bs))
+#'
+#' # Raster subset
+#' raster_subset(bs, polygon = ca_polygon)
+#' }
 raster_subset <- function(
   raster,
-  longitude,
-  latitude,
-  radius = 20000
+  ...
 ) {
 
-  # Checks
-  MazamaCoreUtils::stopIfNull(raster)
-  MazamaCoreUtils::stopIfNull(longitude)
-  MazamaCoreUtils::stopIfNull(latitude)
-  MazamaCoreUtils::stopIfNull(radius)
+  args <- list(...)
 
-  # Convert raster object to data frame
-  df <- suppressWarnings(
-    raster::rasterToPoints(raster)
-  )
+  if ( 'radius' %in% names(args) ) {
+    if ( 'longitude' %in% names(args) && 'latitude' %in% names(args) ) {
 
-  # Calculate distances from target latitiude and longitude
-  target_dist <- geosphere::distGeo(p1 = c(longitude, latitude), p2 = df[,1:2])
+      # Convert raster object to data frame
+      df <- suppressWarnings(
+        raster::rasterToPoints(raster)
+      )
+      r <- args[['radius']]
+      lat <- args[['latitude']]
+      lon <- args[['longitude']]
+      # Calculate distances from target latitiude and longitude
+      target_dist <- geosphere::distGeo(p1 = c(lon, lat), p2 = df[,1:2])
+      # Subset around coordinates in given radius
+      in_radius <- df[which(target_dist <= r),]
+      # Convert subsetted df back to raster
+      ras <- suppressWarnings(
+        raster::rasterFromXYZ(in_radius)
+      )
 
-  # Subset around coordinates in given radius
-  in_radius <- df[which(target_dist <= radius),]
+      return(ras)
 
-  # Convert subsetted df back to raster
-  ras <- suppressWarnings(
-    raster::rasterFromXYZ(in_radius)
-  )
+    } else {
+      stop('Radial subsetting requires a target coordinates - latitude, longitude.')
+    }
 
-  return(ras)
+  } else if ( 'polygon' %in% names(args) ) {
+
+    p <- args[['polygon']]
+    # Mask using spatial polygon
+    ras <- suppressWarnings(
+      raster::mask(raster, p)
+    )
+
+    return(ras)
+
+  } else {
+    stop('Missing subset arguments.')
+  }
+
+  if ( FALSE ) {
+    raster <- X1
+    args <- list('latitude' = 45, 'longitude' = 118, 'radius' = 20000)
+    raster_subset(raster, radius = 20000, latitude = 45, longitude = 118)
+    raster_subset(raster, polygon = p)
+  }
 
 }
