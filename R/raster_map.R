06147a79a9d3541cc6f1cb41b0345d2fe285d237
Hans <hansmrtn@gmail.com>
2020-03-26 15:02:51 -0700

    updated to S3

--- R/raster_map.R
+++ R/raster_map.R
@@ -25,99 +25,121 @@
 #' bs <- raster_load()
 #' raster_map(bs)
 #' }
-raster_map <-
-  function(
-    raster,
-    index = 1,
-    palette = 'Greys',
-    breaks = 'default',
-    title = 'PM2.5',
-    direction = 1,
-    timezone = 'UTC',
-    verbose = TRUE
-  ) {
+raster_map <- function( raster,
+                        index = 1,
+                        palette = 'Greys',
+                        breaks = 'default',
+                        title = 'PM2.5',
+                        direction = 1,
+                        timezone = 'UTC',
+                        verbose = TRUE ) {
+  UseMethod('raster_map', raster)
+}
 
-    # NOTE: Sub-internal function for plotting an individual ggplot2 raster layer
-    .plot_map <- function(r) {
-      # Get layer coordinate limits
-      limits <- raster::extent(r)
-      xlim <- c(limits@xmin, limits@xmax)
-      ylim <- c(limits@ymin, limits@ymax)
+#' @describeIn raster_map A Raster object implementation.
+raster_map.Raster <-function( raster,
+                              index = 1,
+                              palette = 'Greys',
+                              breaks = 'default',
+                              title = 'PM2.5',
+                              direction = 1,
+                              timezone = 'UTC',
+                              verbose = TRUE ) {
 
-      layer_time <- as.numeric(stringr::str_remove(names(r), 'X'))
-      class(layer_time) <- c('POSIX', 'POSIXct')
-      attr(layer_time, 'tzone') <- timezone
-      layer_time <- as.character.Date(layer_time)
+  if ( verbose ) message('A raster')
+  if ( index > dim(raster)[3] ) {
+    stop('Index out of range.')
+  }
+  layer <- raster[[index]]
+  .plot_map(layer, title, timezone, breaks, direction, palette)
 
+}
 
-      # Define color breaks
-      if ( breaks == 'default' ) {
-        breaks <- c(0,12, 35, 55, 150, 250, 350, 500)
-      }
+#' @describeIn raster_map A multithreaded implementation for iterating over lists.
+raster_map.list <- function( raster,
+                             index = 1,
+                             palette = 'Greys',
+                             breaks = 'default',
+                             title = 'PM2.5',
+                             direction = 1,
+                             timezone = 'UTC',
+                             verbose = TRUE ) {
 
-      # Get state and its counties to map
-      states <- ggplot2::map_data('state', xlim = xlim, ylim = ylim)
-      counties <- ggplot2::map_data('county', xlim = xlim, ylim = ylim)
+  if ( verbose ) {
+    message(paste0('Plotting ', length(raster), ' models ...'))
+  }
 
-      # ggplot it
-      gg <- rasterVis::gplot(r) +
-        ggplot2::geom_raster(ggplot2::aes(fill = cut(.data$value, breaks))) +
-        ggplot2::geom_path(data = counties,
-                           ggplot2::aes( x = .data$long,
-                                         y = .data$lat,
-                                         group = .data$group ),
-                           alpha = 0.2,
-                           color = 'grey12' ) +
-        ggplot2::geom_polygon( data = states,
-                               fill = 'NA',
-                               color = 'black',
-                               ggplot2::aes( y = .data$lat,
-                                             x = .data$long,
-                                             group = .data$group ),
-        ) +
-        ggplot2::scale_fill_brewer( na.value = NA,
-                                    palette = palette,
-                                    direction = direction ) +
-        ggplot2::theme_classic() +
-        ggplot2::labs(title = title,
-                      subtitle = layer_time,
-                      x = 'Longitude', y = 'Latitude', fill = 'PM2.5') +
-        ggplot2::coord_fixed(xlim = xlim, ylim = ylim, ratio = 4/3)
-
-      return(gg)
-    }
-
-    if ( class(raster) == 'list' ) {
-      if ( verbose ) message(paste0('Plotting ', length(raster), ' models ...'))
-      layer_list <- lapply(
-        X = raster,
-        FUN = function(r) {
-          if ( index > dim(r)[3] ) {
-            stop('Index out of range.')
-          }
-          return(r[[index]])
-        }
-      )
-
-      cl <- parallel::makeCluster(future::availableCores() - 1)
-      future::plan(strategy = future::cluster, workers = cl)
-      gg_list <- future.apply::future_lapply(
-        X = layer_list,
-        FUN = function(r) {
-          .plot_map(r)
-        }
-      )
-      parallel::stopCluster(cl)
-
-      return(cowplot::plot_grid(plotlist = gg_list))
-
-    } else if ( stringr::str_detect(class(raster), 'Raster*') ) {
-      if ( verbose ) message('A raster')
-      if ( index > dim(raster)[3] ) {
+  layer_list <- lapply(
+    X = raster,
+    FUN = function(r) {
+      if ( index > dim(r)[3] ) {
         stop('Index out of range.')
       }
-      layer <- raster[[index]]
-      .plot_map(layer)
+      return(r[[index]])
     }
+  )
 
+  cl <- parallel::makeCluster(future::availableCores() - 1)
+  future::plan(strategy = future::cluster, workers = cl)
+  gg_list <- future.apply::future_lapply(
+    X = layer_list,
+    FUN = function(r) {
+      .plot_map(r, title, timezone, breaks, direction, palette)
+    }
+  )
+  parallel::stopCluster(cl)
+
+  return(cowplot::plot_grid(plotlist = gg_list))
+
+}
+
+# NOTE: Sub-internal function for plotting an individual ggplot2 raster layer
+.plot_map <- function(raster, title, timezone, breaks, direction, palette) {
+  # Get layer coordinate limits
+  limits <- raster::extent(raster)
+  xlim <- c(limits@xmin, limits@xmax)
+  ylim <- c(limits@ymin, limits@ymax)
+
+  layer_time <- as.numeric(stringr::str_remove(names(raster), 'X'))
+  class(layer_time) <- c('POSIX', 'POSIXct')
+  attr(layer_time, 'tzone') <- timezone
+  layer_time <- as.character.Date(layer_time)
+
+  # Define color breaks
+  if ( breaks == 'default' ) {
+    breaks <- c(0,12, 35, 55, 150, 250, 350, 500)
   }
+
+  # Get state and its counties to map
+  states <- ggplot2::map_data('state', xlim = xlim, ylim = ylim)
+  counties <- ggplot2::map_data('county', xlim = xlim, ylim = ylim)
+
+  # ggplot it
+  gg <- rasterVis::gplot(raster) +
+    ggplot2::geom_raster(ggplot2::aes(fill = cut(.data$value, breaks))) +
+    ggplot2::geom_path(data = counties,
+                       ggplot2::aes( x = .data$long,
+                                     y = .data$lat,
+                                     group = .data$group ),
+                       alpha = 0.2,
+                       color = 'grey12' ) +
+    ggplot2::geom_polygon( data = states,
+                           fill = 'NA',
+                           color = 'black',
+                           ggplot2::aes( y = .data$lat,
+                                         x = .data$long,
+                                         group = .data$group ),
+    ) +
+    ggplot2::scale_fill_brewer( na.value = NA,
+                                palette = palette,
+                                direction = direction ) +
+    ggplot2::theme_classic() +
+    ggplot2::labs(title = title,
+                  subtitle = layer_time,
+                  x = 'Longitude', y = 'Latitude', fill = 'PM2.5') +
+    ggplot2::coord_fixed(xlim = xlim, ylim = ylim, ratio = 4/3)
+
+  return(gg)
+
+}
+
